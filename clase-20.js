3d86cfdebf880011b742dff1b2d68235077915c8
Ginna Mora <higinnamora@gmail.com>
2021-02-01 13:33:05 -0500

    Clase 20 - Reducir un array a un valor

---
+++ clase-20.js
@@ -0,0 +1,86 @@
+// Reducir un array a un valor
+
+var sacha = {
+  nombre: 'Sacha',
+  apellido: 'Lifszyc',
+  altura: 1.72
+}
+
+var alan = {
+  nombre: 'Alan',
+  apellido: 'Perez',
+  altura: 1.86
+}
+
+var martin = {
+  nombre: 'Martin',
+  apellido: 'Gomez',
+  altura: 1.85
+}
+
+var dario = {
+  nombre: 'Dario',
+  apellido: 'Juarez',
+  altura: 1.71
+}
+
+var vicky = {
+  nombre: 'Vicky',
+  apellido: 'Zapata',
+  altura: 1.56
+}
+
+var paula = {
+  nombre: 'Paula',
+  apellido: 'Barros',
+  altura: 1.76
+}
+
+const esAlta = ({ altura }) => {
+  return persona.altura > 1.8
+}
+
+var personas = [sacha, alan, martin, dario, vicky, paula]
+
+var personasAltas = personas.filter(esAlta)
+
+
+const pasarAlturaACms = persona => {
+//  persona.altura = persona.altura * 100
+  persona.altura *= 100
+  return persona 
+}
+
+var personasCms = personas.map(pasarAlturaACms)
+
+console.log(personasAltas)
+
+const esAlta = ({ altura }) => altura > 1.8
+
+var personas = [sacha, alan, martin, dario, vicky, paula]
+
+
+var personasAltas = personas.filter(esAlta)
+// var personasAltas = personas.filter(function (persona) {
+//   return persona.altura > 1.8
+// })
+
+// persona.altura = persona.altura * 100
+const pasarAlturaACms = persona => ({
+  ...persona,
+  altura: persona.altura * 100
+})
+
+var personasCms = personas.map(pasarAlturaACms)
+
+// var acum = 0
+//
+// for (var i = 0; i < personas.length; i++) {
+//   acum = acum + personas[i].cantidadDeLibros
+// }
+
+const reducer = (acum, { cantidadDeLibros }) => acum + cantidadDeLibros
+
+var totalDeLibros = personas.reduce(reducer, 0)
+
+console.log(`En total todos tienen ${totalDeLibros} libros`)
